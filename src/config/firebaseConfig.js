472c23532b824f83c27f5224389b41a41dad623b
Rohitsrt <rohitkumar224@gmail.com>
2024-05-19 10:56:14 +0530

    Completed

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSHKbb4KPV2IFGqGY8qSAS7LXSvI_3cVQ",
+  authDomain: "crypto-stock-tracker.firebaseapp.com",
+  projectId: "crypto-stock-tracker",
+  storageBucket: "crypto-stock-tracker.appspot.com",
+  messagingSenderId: "134810056047",
+  appId: "1:134810056047:web:737ab4c954848478a2649a",
+  measurementId: "G-5GXPV07SZ1"
+};
+
+export default firebaseConfig;
